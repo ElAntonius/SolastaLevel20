4a711e8989b977e6c467a23424e302309e58a32f
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-13 14:28:37 -0700

    patch for new UpdateLevelCaps method

---
+++ SolastaLevel20/Patches/GameCampaignPartyPatcher.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using static SolastaLevel20.Settings;
+
+namespace SolastaLevel20.Patches
+{
+    class GameCampaignPartyPatcher
+    {
+        [HarmonyPatch(typeof(GameCampaignParty), "UpdateLevelCaps")]
+        internal static class GameCampaignParty_UpdateLevelCaps_Patch
+        {
+            internal static bool Prefix(GameCampaignParty __instance)
+            {
+                int levelCap = MOD_MAX_LEVEL;
+                foreach (GameCampaignCharacter characters in __instance.CharactersList)
+                {
+                    RulesetAttribute attribute1 = characters.RulesetCharacter.GetAttribute("CharacterLevel");
+                    attribute1.MaxValue = levelCap > 0 ? levelCap : 13;
+                    attribute1.Refresh();
+                    RulesetAttribute attribute2 = characters.RulesetCharacter.GetAttribute("Experience");
+                    attribute2.MaxValue = HeroDefinitions.MaxHeroExperience(attribute1.MaxValue);
+                    attribute2.Refresh();
+                }
+                return false;
+            }
+        }
+    }
+}
