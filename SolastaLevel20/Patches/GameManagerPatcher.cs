883273dd53efa763e4c995907d3fbcf92ee99695
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-25 09:04:42 -0700

    fix progression from 19 to 20

--- SolastaLevel20/Patches/GameManagerPatcher.cs
+++ SolastaLevel20/Patches/GameManagerPatcher.cs
@@ -1,10 +1,36 @@
 ﻿using HarmonyLib;
 using SolastaLevel20.Models.Classes;
+using static SolastaLevel20.Settings;
 
 namespace SolastaLevel20.Patches
 {
     class GameManagerPatcher
     {
+        internal static void FixCastSpellTables()
+        {
+            var featureDefinitionCastSpellDB = DatabaseRepository.GetDatabase<FeatureDefinitionCastSpell>();
+
+            foreach (var featureDefinitionCastSpell in featureDefinitionCastSpellDB)
+            {
+                while (featureDefinitionCastSpell.KnownCantrips.Count < MOD_MAX_LEVEL + 1)
+                {
+                    featureDefinitionCastSpell.KnownCantrips.Add(0);
+                }
+                while (featureDefinitionCastSpell.KnownSpells.Count < MOD_MAX_LEVEL + 1)
+                {
+                    featureDefinitionCastSpell.KnownSpells.Add(0);
+                }
+                while (featureDefinitionCastSpell.ScribedSpells.Count < MOD_MAX_LEVEL + 1)
+                {
+                    featureDefinitionCastSpell.ScribedSpells.Add(0);
+                }
+                while (featureDefinitionCastSpell.ReplacedSpells.Count < MOD_MAX_LEVEL + 1)
+                {
+                    featureDefinitionCastSpell.ReplacedSpells.Add(0);
+                }
+            }
+        }
+
         [HarmonyPatch(typeof(GameManager), "BindPostDatabase")]
         internal static class GameManager_BindPostDatabase_Patch
         {
@@ -17,10 +43,13 @@ namespace SolastaLevel20.Patches
                 PaladinBuilder.Load();
                 RangerBuilder.Load();
                 RogueBuilder.Load();
+                SorcererBuilder.Load();
                 WizardBuilder.Load();
 
                 MartialSpellBladeBuilder.Load();
                 MartialSpellBladeBuilder.Load();
+
+                FixCastSpellTables();
             }
         }
     }
